57f847fdff451ba23def9b409839dbf3a0b0c904
Victor Lima <victoraamlima@gmail.com>
2023-03-28 22:36:44 -0300

    refactor of the file

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Home } from "./home"
+import { Home } from "./home/home"
 import { PokemonPage } from "./pokemonPage"
 
 const AppRoutes = () => (
     
-    <BrowserRouter data-testid="AppRoutes">
+    <BrowserRouter>
         <Routes>
             <Route exact path="/pokemons" element={<Home />} />
             <Route exact path="/pokemon/:name" element={<PokemonPage />} />
