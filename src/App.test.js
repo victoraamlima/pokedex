c0d3f533a305e96ed7af93d35d93e71765dd7064
Victor Lima <victoraamlima@gmail.com>
2023-03-28 22:38:39 -0300

    add of the first try of test

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,10 @@
-import { getByTestId, render, screen } from '@testing-library/react';
-import App from './App';
-import { AppRoutes } from "./pages/routes"
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-// test("render App", () =>{
-
-// })
+// import { getByTestId, render, screen } from '@testing-library/react';
+// import App from './App';
 
 test('renders app correctly', () => {
-  const { getByTestId } = render(<App />);
-  // const linkElement = getByText(/Hello, World!/i);
-  const linkElement = getByTestId("AppRoutes");
-  expect(linkElement).toBeInTheDocument();
+  // render(<App/>)
+
+  // const element = screen.getByTestId("custom-element")
+  // expect(screen.getByText("test")).toBeInTheDocument()
+
 });
