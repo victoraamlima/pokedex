3cb24a2eb3a65109c147a59a9f03daadb0ab33ef
Victor Lima <victoraamlima@gmail.com>
2023-03-30 14:20:33 -0300

    add style scrollbar

--- src/style/style.js
+++ src/style/style.js
@@ -5,12 +5,33 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        
+    }
+
+    body::-webkit-scrollbar {
+        // width: 17px;
+        width: 3vw;
+        // border: 0.1px solid #000;
+    }
+
+    body::-webkit-scrollbar-track {
+        // background: orange;
+    }
+      
+    body::-webkit-scrollbar-thumb {
+        background-color: #FF4D03;
+        // border-radius: 20px;
+        // border: 3px solid orange;
     }
 
     @font-face {
         font-family: 'Pokemon';
-        src: local('Pokemon'), url(../fonts/Pokemon-Solid.ttf) format('ttf');
-      }
+        src: local('Pokemon'), url('../fonts/pokemon-solid.ttf') format('ttf'), url('../fonts/pokemon-solid.woff') format('woff'), ;
+    }
+
+     
+    
+   
 `;
 
 export { GlobalStyle };
