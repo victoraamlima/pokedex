5efb8c011b02f2760e32b1deaf61d702dc8f4331
Victor Lima <victoraamlima@gmail.com>
2023-03-30 14:22:09 -0300

    fix header style

--- src/components/header/styled.js
+++ src/components/header/styled.js
@@ -1,12 +1,11 @@
 import styled from "styled-components"
 
 const StyledHeader = styled.header`
-background: linear-gradient(180deg, #E84539 0%, #9F1910 100%);
+// background: linear-gradient(180deg, #E84539 0%, #9F1910 100%);
 display: flex; 
-height: 85px;
+width: 100vw;
 justify-content: space-between;
-align-items: center;
-padding: 0 20px;
+padding-top: 20px;
 position: relative;
 `
 
